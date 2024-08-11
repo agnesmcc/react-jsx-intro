8a3e8449adda58f642da7257f9a94fde72449d8e
Agnes <agnes.mccall5@gmail.com>
2024-08-10 21:50:00 -0400

    add FirstComponent

---
+++ part-1/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div>
+            <FirstComponent/>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
